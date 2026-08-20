91d2bd2d2e6803becb5f4d618a224ad1bd4723cc
Pratyakksh <prathyaksh24@gmail.com>
2026-08-20 20:16:15 +0530

    Add Store catalog so terminal tools can be found and detected

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,17 @@
+mod catalog;
 mod pty;
 
+use catalog::CatalogRuntime;
 use pty::SessionManager;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
+        .manage(CatalogRuntime::builtins())
         .manage(SessionManager::default())
         .invoke_handler(tauri::generate_handler![
+            catalog::catalog_detect,
+            catalog::catalog_list,
             pty::create_session,
             pty::write_session,
             pty::resize_session,
